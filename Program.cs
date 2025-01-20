01e830631186e22ef0d76d94b397c585eb1bccf2
Gisellvanegas <productofficerads@fundacioncapital.org>
2025-01-19 23:07:46 -0500

    comit inicial

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Componente_de_pokemon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
